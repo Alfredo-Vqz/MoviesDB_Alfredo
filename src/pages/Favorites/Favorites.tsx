29aab8c6fa3d0a9d77139413fe8415706b51cfbf
Alfredo-Vqz <a01656384@tec.mx>
2024-05-05 15:17:24 -0600

    fix favorites

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -35,8 +35,8 @@ const Favorites = () => {
     return (
         <div className="bg-gray-800 h-screen">
             <div className='flex justify-center'>
-                {loading && <div>Loading...</div>}
                 {shows.length === 0 && !loading && <div className="font-bold text-white opacity-80 font-serif text-3xl mt-10 mr-auto ml-80"> It looks like you haven't added any favorite movies yet.</div>}
+                {loading && <div>Loading...</div>}
                 {shows.length > 0 && (
                     <div className='grid grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-8'>
                         {shows.map((movie: IMovieDetail) => (
