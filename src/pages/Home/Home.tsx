10edd43a6cecc252d40ef64391a4072dd841e87b
Alfredo-Vqz <a01656384@tec.mx>
2024-05-05 14:21:06 -0600

    arreglar error de map para el deployment en vercel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,9 @@ import { MovieCard } from "../../components/MovieCard";
 import "./Home.css"
 
 const Home: React.FC = () => {
-    const [popularMovies, setPopularMovies] = useState<IMovieResponse[]>([]);
-    const [topRatedMovies, setTopRatedMovies] = useState<IMovieResponse[]>([]);
-    const [upcomingMovies, setUpcomingMovies] = useState<IMovieResponse[]>([]);
+    const [popularMovies, setPopularMovies] = useState<IMovieResponse[] | undefined>(undefined);
+    const [topRatedMovies, setTopRatedMovies] = useState<IMovieResponse[] | undefined>(undefined);
+    const [upcomingMovies, setUpcomingMovies] = useState<IMovieResponse[] | undefined>(undefined);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
 
@@ -38,60 +38,66 @@ const Home: React.FC = () => {
         <div className="bg-gray-800">
             {loading && <div>Loading...</div>}
             {error && <div>Error...</div>}
-            
-            <div className="movie-section px-6 py-8">
-                <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Popular</div>
-                <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
-                    <div className="flex ml-5 mt-5">
-                        {popularMovies.map((movie) => (
-                            <MovieCard
-                                key={movie.id}
-                                movieId={movie.id}
-                                posterPath={movie.poster_path}
-                                title={movie.title}
-                                voteAverage={movie.vote_average}
-                                genreId={movie.genre_ids[0]}
-                            />
-                        ))}
+                        {popularMovies?.length ? (
+                <div className="movie-section px-6 py-8">
+                    <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Popular</div>
+                    <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
+                        <div className="flex ml-5 mt-5">
+                            {/* Map over popular movies and render MovieCard component */}
+                            {popularMovies.map((movie) => (
+                                <MovieCard
+                                    key={movie.id}
+                                    movieId={movie.id}
+                                    posterPath={movie.poster_path}
+                                    title={movie.title}
+                                    voteAverage={movie.vote_average}
+                                    genreId={movie.genre_ids[0]}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : null}
 
-            <div className="movie-section px-6 py-2">
-                <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Top Rated</div>
-                <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
-                    <div className="flex ml-5 mt-5">
-                        {topRatedMovies.map((movie) => (
-                            <MovieCard
-                                key={movie.id}
-                                movieId={movie.id}
-                                posterPath={movie.poster_path}
-                                title={movie.title}
-                                voteAverage={movie.vote_average}
-                                genreId={movie.genre_ids[0]}
-                            />
-                        ))}
+            {topRatedMovies?.length ? (
+                <div className="movie-section px-6 py-2">
+                    <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Top Rated</div>
+                    <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
+                        <div className="flex ml-5 mt-5">
+                            {topRatedMovies.map((movie) => (
+                                <MovieCard
+                                    key={movie.id}
+                                    movieId={movie.id}
+                                    posterPath={movie.poster_path}
+                                    title={movie.title}
+                                    voteAverage={movie.vote_average}
+                                    genreId={movie.genre_ids[0]}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : null}
 
-            <div className="movie-section px-6 py-2">
-                <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Upcoming</div>
-                <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
-                    <div className="flex ml-5 mt-5">
-                        {upcomingMovies.map((movie) => (
-                            <MovieCard
-                                key={movie.id}
-                                movieId={movie.id}
-                                posterPath={movie.poster_path}
-                                title={movie.title}
-                                voteAverage={movie.vote_average}
-                                genreId={movie.genre_ids[0]}
-                            />
-                        ))}
+\            {upcomingMovies?.length ? (
+                <div className="movie-section px-6 py-2">
+                    <div className="font-bold text-white font-serif text-3xl mb-4 ml-4">Upcoming</div>
+                    <div className="slider-container flex overflow-x-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
+                        <div className="flex ml-5 mt-5">
+                            {upcomingMovies.map((movie) => (
+                                <MovieCard
+                                    key={movie.id}
+                                    movieId={movie.id}
+                                    posterPath={movie.poster_path}
+                                    title={movie.title}
+                                    voteAverage={movie.vote_average}
+                                    genreId={movie.genre_ids[0]}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : null}
         </div>
     );
 };
